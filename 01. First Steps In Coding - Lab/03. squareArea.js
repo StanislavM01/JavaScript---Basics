52ccbeeb33b7867925930991099596e903c2de48
StanislavM01 <81364482+StanislavM01@users.noreply.github.com>
2021-03-25 19:15:29 +0200

    Add files via upload

---
+++ 01. First Steps In Coding - Lab/03. squareArea.js
@@ -0,0 +1,6 @@
+function kvadrat(input){
+let a = Number(input[0])
+let liceto = a * a
+console.log(liceto)
+}
+kvadrat(['5'])
---
+++ 01. First Steps In Coding - Lab/04. inchesToCentimeters.js
@@ -0,0 +1,8 @@
+function inches(input){
+let oneInchInCm = 2.54;
+let result = Number(input[0]) * oneInchInCm;
+console.log(result);
+
+
+}
+inches(['5']);
